c5d5633353cfc935fdef2f39fc820f955a1be42d
Arvind Panwar <arvindpanwar706@gmail.com>
2024-01-23 22:03:39 +0530

    Update adjacency_matrix.cpp

--- dsa/graph/adjacency_matrix.cpp
+++ dsa/graph/adjacency_matrix.cpp
@@ -3,102 +3,82 @@
 
 using namespace std;
 
-class Graph {
-private:
-    int numVertices;
-    vector<vector<int>> adjList;
-
-public:
-    Graph(int n) {
-        numVertices = n;
-        adjList.resize(numVertices);
-    }
-
-    void addEdge(int src, int dest) {
-        adjList[src].push_back(dest);
-        adjList[dest].push_back(src);
-    }
-
-    void deleteEdge(int src, int dest) {
-        // Find the index of the destination vertex in the source vertex's adjacency list
-        int destIndex = -1;
-        for (int i = 0; i < adjList[src].size(); i++) {
-            if (adjList[src][i] == dest) {
-                destIndex = i;
-                break;
-            }
-        }
-        // Remove the destination vertex from the source vertex's adjacency list
-        if (destIndex != -1) {
-            adjList[src].erase(adjList[src].begin() + destIndex);
-        }
-
-        // Find the index of the source vertex in the destination vertex's adjacency list
-        int srcIndex = -1;
-        for (int i = 0; i < adjList[dest].size(); i++) {
-            if (adjList[dest][i] == src) {
-                srcIndex = i;
-                break;
-            }
-        }
-        // Remove the source vertex from the destination vertex's adjacency list
-        if (srcIndex != -1) {
-            adjList[dest].erase(adjList[dest].begin() + srcIndex);
-        }
-    }
-
-    bool isConnected(int src, int dest) {
-        for (int i = 0; i < adjList[src].size(); i++) {
-            if (adjList[src][i] == dest) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void print() {
-        for (int i = 0; i < numVertices; i++) {
-            cout << i << ": ";
-            for (int j = 0; j < adjList[i].size(); j++) {
-                cout << adjList[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
+class Graph{
+    //data member
+    public: 
+        int numVertices;
+        vector<vector<int>> adjMatrix;
+        
+        // Constructor
+        Graph(int numVertices);
+       
+        //member function
+        void addEdge(int src, int dest);
+        void deleteEdge(int src, int dest);
+        bool isConnected(int src, int dest);
+        void print();
 };
 
+Graph::Graph(int numVertices) {
+    this->numVertices = numVertices;
+    
+    // Initialize adjacency matrix to all zeros
+    for (int i = 0; i < numVertices; i++) {
+        vector<int> row(numVertices, 0);
+        adjMatrix.push_back(row);
+    }
+}
+
+// Add edge between two vertices with unit weight
+void Graph::addEdge(int src, int dest) {
+    adjMatrix[src][dest] = 1;
+}
+
+// Delete edge between two vertices
+void Graph::deleteEdge(int src, int dest) {
+    adjMatrix[src][dest] = 0;
+}
+
+// Check if two vertices are directly connected
+bool Graph::isConnected(int src, int dest) {
+    return (adjMatrix[src][dest] == 1);
+}
+
+// Print out adjacency matrix
+void Graph::print() {
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
+            cout << adjMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    Graph g(5);
+   Graph g(5);
 
     g.addEdge(0, 1);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
+    g.addEdge(0, 2);
     g.addEdge(1, 3);
-    g.addEdge(1, 4);
     g.addEdge(2, 3);
     g.addEdge(3, 4);
 
-    g.print();
-    // Output:
-    // 0: 1 4 
-    // 1: 0 2 3 4 
-    // 2: 1 3 
-    // 3: 1 2 4 
-    // 4: 0 1 3 
+    g.print();  // 0 1 1 0 0 
+                // 0 0 0 1 0 
+                // 0 0 0 1 0 
+                // 0 0 0 0 1 
+                // 0 0 0 0 0
+    cout << endl;
+    g.deleteEdge(2, 3);
+    cout << "Deleting edge (2,3)" << endl;
+    g.print();  // 0 1 1 0 0 
+                // 0 0 0 1 0 
+                // 0 0 0 0 0 
+                // 0 0 0 0 1 
+                // 0 0 0 0 0
+    cout << endl << "Check edge connected " << endl;
+    cout << g.isConnected(0, 2) << endl;  // true
+    cout << g.isConnected(0, 3) << endl;  // false
 
-    g.deleteEdge(1, 4);
-
-    g.print();
-    // Output:
-    // 0: 1 4 
-    // 1: 0 2 3 
-    // 2: 1 3 
-    // 3: 1 2 4 
-    // 4: 0 3 
-
-    cout << g.isConnected(0, 1) << endl; // Output: true
-    cout << g.isConnected(0, 2) << endl; // Output: false
- 
     return 0;
-
 }
