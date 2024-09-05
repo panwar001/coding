3426907e92aca8a72353001c36909ace02cca6ee
Arvind Panwar <arvindpanwar706@gmail.com>
2024-09-05 14:37:06 +0530

    Update print_all_subsequences.cpp

--- dsa/print_all_subsequences.cpp
+++ dsa/print_all_subsequences.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printSubsequence() {
+void printSubsequence(vector<int> &ds, int n, int index) {
+
+  return;
 }
 
 int main() {
+  vector<int> arr  = {3,1,2,4};
+  int n = 4;
+  printSubsequence(ds, n, 0);
   return 0;
 }
